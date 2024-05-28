0a7e02792edc7f185d02fb37d139f34437209153
katrina <katrinablazkova2006@gmail.com>
2024-05-28 20:00:50 +0300

    Turpinu projekta izstrādi

--- src/Picerija.java
+++ src/Picerija.java
@@ -10,6 +10,15 @@ public class Picerija {
         String izmers;
         List<String> toppingi;
         double cena;
+        
+        public Pasutijums(String vards, String telefons, String pica, String izmers, List<String> toppingi, double cena) {
+            this.vards = vards;
+            this.telefons = telefons;
+            this.pica = pica;
+            this.izmers = izmers;
+            this.toppingi = toppingi;
+            this.cena = cena;
+        }
 
 	}
 
